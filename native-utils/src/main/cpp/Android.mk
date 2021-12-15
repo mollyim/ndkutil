ee737212b66521c45167380178d6a4d0c4dd485a
Oscar Mira <valldrac@molly.im>
2021-12-15 17:21:26 +0100

    Import native-utils

---
+++ native-utils/src/main/cpp/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE     := native-utils
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_CFLAGS     += -Wall
+ifeq ($(APP_OPTIM),release)
+LOCAL_LDLIBS     += -Wl,--build-id=none
+endif
+
+LOCAL_SRC_FILES := org_thoughtcrime_securesms_util_FileUtils.cpp
+LOCAL_SRC_FILES += org_thoughtcrime_securesms_service_WipeMemoryService.c
+
+include $(BUILD_SHARED_LIBRARY)
